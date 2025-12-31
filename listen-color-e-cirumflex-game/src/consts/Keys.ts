694f379aacd70465944074fbca794f47a61f2059
DungLa <leanhdung3105@gmail.com>
2025-12-31 09:51:38 +0700

    feat/listen-color-e-cỉumflex-game(Ê): clone project A structure and update data images, audio

---
+++ listen-color-e-cirumflex-game/src/consts/Keys.ts
@@ -0,0 +1,75 @@
+// src/consts/Keys.ts
+
+// 1. Tên các Màn chơi (Scene)
+export enum SceneKeys {
+    Preload = 'PreloadScene',
+    Scene1 = 'Scene1',
+    Scene2 = 'Scene2',
+    EndGame = 'EndGameScene'
+}
+
+// 2. Tên các Hình ảnh (Texture)
+export enum TextureKeys {
+    // --- UI Dùng Chung ---
+    BtnExit = 'btn_exit',
+    BtnReset = 'btn_reset',
+    BtnEraser = 'btn_eraser',
+    HandHint = 'hand_hint',
+    BgPopup = 'bg_popup', // board_pop_up dùng chung
+    
+    // --- Scene 1 (Thước kẻ) ---
+    S1_BannerBg = 'banner_bg_s1',
+    S1_BannerText = 'banner_text_s1',
+    S1_Board = 'board_white',
+    S1_PoemText = 'poem_text',
+    S1_Illustration = 'img_illustration',
+    S1_IconOHeader = 'icon_o_header',
+    S1_Item_1 = 's1_item_1',
+    S1_Item_2 = 's1_item_2',
+    S1_Item_3 = 's1_item_3',
+    S1_TextResult = 'text_result_s1',
+
+    // --- Scene 2 (Tô Màu) ---
+    S2_Banner = 'banner_s2',
+    S2_TextBanner = 'text_banner_s2',
+    S2_Board = 'board_s2',
+    
+    // Các bộ phận tô màu
+    S2_A_Outline = 'e_outline',
+    S2_A_Body = 'e_body',
+    S2_A_Hat = 'e_hat',
+    
+    S2_Text_Item = 'text_item',
+    S2_Item_Outline = 'item_outline',
+    S2_Item_1 = 's2_item_1',
+    S2_Item_2 = 's2_item_2',
+    S2_Item_3 = 's2_item_3',
+    S2_Item_4 = 's2_item_4',
+    S2_Item_5 = 's2_item_5',
+    S2_Item_6 = 's2_item_6',
+    S2_Item_7 = 's2_item_7',
+    S2_Item_8 = 's2_item_8',
+
+    // Các nút màu
+    BtnRed = 'btn_red',
+    BtnYellow = 'btn_yellow',
+    BtnGreen = 'btn_green',
+    BtnBlue = 'btn_blue',
+    BtnPurple = 'btn_purple',
+    BtnCream = 'btn_cream',
+    BtnBlack = 'btn_black',
+
+    // --- End Game ---
+    End_Icon = 'icon_end',
+    End_BannerCongrat = 'banner_congrat'
+}
+
+// 3. Tên Âm thanh (Audio)
+export enum AudioKeys {
+    BgmNen = 'bgm-nen'
+}
+
+// 4. Tên File Data (JSON)
+export enum DataKeys {
+    LevelS2Config = 'level_config'
+}
